9685311f34450a37b95b1debc96d3a890b1d67a9
Santiago Avila <sanavila2002@gmail.com>
2025-01-30 21:16:31 -0500

    Initial commit: Api Rest project setup

---
+++ bodegon-api/src/main/java/com/savisoft/bodegon_api/BodegonApiApplication.java
@@ -0,0 +1,13 @@
+package com.savisoft.bodegon_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodegonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodegonApiApplication.class, args);
+	}
+
+}
---
+++ bodegon-api/src/test/java/com/savisoft/bodegon_api/BodegonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savisoft.bodegon_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodegonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
